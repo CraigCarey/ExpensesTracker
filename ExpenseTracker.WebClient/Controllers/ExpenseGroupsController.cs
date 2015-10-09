71593ae3b8a3db09d9a548cae62ed1a270af12af
Craig Carey <sgccarey@gmail.com>
2015-10-09 16:03:11 +0100

    Added delete functionality

--- ExpenseTracker.WebClient/Controllers/ExpenseGroupsController.cs
+++ ExpenseTracker.WebClient/Controllers/ExpenseGroupsController.cs
@@ -150,9 +150,27 @@ namespace ExpenseTracker.WebClient.Controllers
          
 
         // POST: ExpenseGroups/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
-            return View();
+            try
+            {
+                var client = ExpenseTrackerHttpClient.GetClient();
+                
+                var response = await client.DeleteAsync("api/expensegroups/" + id);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return Content("An error occurred");
+                }
+            }
+            catch
+            {
+                return Content("An error occurred.");
+            }
         }
     }
 }
